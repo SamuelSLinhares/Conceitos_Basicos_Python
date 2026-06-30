1b063f62a4c229deb56aff9fdfdf0e6e7270138a
Samuel Linhares <samuel.linhares@academico.ifpb.edu.br>
2026-06-30 23:47:23 +0000

    .

---
+++ programas/leitura_arquivos.py
@@ -0,0 +1,21 @@
+# readlines lê todas as linhas de uma vez e retorna uma lista
+#.strip() exclui os espações em branco no início e fim das strings
+
+#with open('arquivos/notas.txt', 'r') as arquivo:
+#    for linha in arquivo:
+#        linha_limpa = linha.strip()
+#        print(linha_limpa)
+
+# exemplo prático
+with open("arquivos/notas.txt", "r") as arquivo:
+    soma = 0
+    contado = 0
+
+    for linha in arquivo:
+        temperatura = float(linha.strip())
+        soma += temperatura
+        contador += 1
+
+    if contador > 0:
+        media = soma / contador
+        print(f'A temperatura média foi: {media:.1f}°C')
